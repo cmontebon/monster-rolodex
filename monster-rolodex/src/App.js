2021ad703df39a4c63a93342b85c23a191969788
cm <cpmontebon25@gmail.com>
2020-09-12 23:53:11 +0800

    Project Monster Rolodex

---
+++ monster-rolodex/src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import { CardList } from './components/card-list/card-list.component';
+
+class App extends Component {
+    constructor() {
+      super()
+
+      this.state = {
+        monsters: []
+      }
+
+    }
+
+    componentDidMount() {
+      fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(data => this.setState({monsters: data}))
+    }
+    
+    render() {
+      return (
+        <div className="App">
+          <CardList monsters={this.state.monsters} />   
+        </div>
+      );
+    }
+}
+
+export default App;
