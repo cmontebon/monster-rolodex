ed4add4a996dfd4c408fdbaecc545b9e5698e278
cm <cpgmontebon@gmail.com>
2020-10-19 00:29:43 +0800

    Updates

---
+++ precache-manifest.d70b248f97f07ff812d2a4f6e016b509.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9eb6d631b406de515c5dbeadf48dab9",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "9613034295bbc988db7c",
+    "url": "/monster-rolodex/static/css/main.936c8f4f.chunk.css"
+  },
+  {
+    "revision": "2d19631dba197ad7891e",
+    "url": "/monster-rolodex/static/js/2.e1627e68.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monster-rolodex/static/js/2.e1627e68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9613034295bbc988db7c",
+    "url": "/monster-rolodex/static/js/main.adea36a2.chunk.js"
+  },
+  {
+    "revision": "212570b3a5f291f8b8f3",
+    "url": "/monster-rolodex/static/js/runtime-main.4a41f6c3.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/monster-rolodex/static/media/logo.ee7cd8ed.svg"
+  }
+]);
